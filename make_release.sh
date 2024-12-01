9c7340bb2b722e803c0fd7132b33c4adbfd421c0
David <davidthemaster30@hotmail.com>
2024-12-01 17:46:31 -0500

    bump verion

--- make_release.sh
+++ make_release.sh
@@ -3,4 +3,4 @@ rm -rf bin
 
 dotnet build RF5Fix.csproj -f net6.0 -c Release
 
-zip -j 'RF5Fix_v0.2.0.zip' './bin/Release/net6.0/RF5Fix.dll' './RF5Fix.cfg'
+zip -j 'RF5Fix_v1.0.0.zip' './bin/Release/net6.0/RF5Fix.dll' './RF5Fix.cfg'
