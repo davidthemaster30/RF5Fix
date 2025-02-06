6a0a7b160f397e9f784fc5fbee65bf32516adcd5
David <davidthemaster30@hotmail.com>
2025-02-05 22:34:02 -0500

    bump version

--- make_release.sh
+++ make_release.sh
@@ -3,7 +3,7 @@ rm -rf bin
 
 dotnet build RF5Fix.csproj -f net6.0 -c Release
 
-cp 'BepInEx-Unity.IL2CPP-win-x64-6.0.0-be.725+e1974e2.zip' 'RF5Fix_v1.0.1.zip'
+cp 'BepInEx-Unity.IL2CPP-win-x64-6.0.0-be.725+e1974e2.zip' 'RF5Fix_v1.0.2.zip'
 
 mkdir -p BepInEx/plugins
 mkdir -p BepInEx/config
@@ -11,6 +11,8 @@ mkdir -p BepInEx/config
 cp './bin/Release/net6.0/RF5Fix.dll' BepInEx/plugins/RF5Fix.dll
 cp './RF5Fix.cfg' BepInEx/config/RF5Fix.cfg
 
-zip -r 'RF5Fix_v1.0.1.zip' BepInEx
+cp './bin/Release/net6.0/RF5Fix.dll' '/data/Steam/steamapps/common/Rune Factory 5/BepInEx/plugins'
+
+zip -r 'RF5Fix_v1.0.2.zip' BepInEx
 
 rm -rf BepInEx
